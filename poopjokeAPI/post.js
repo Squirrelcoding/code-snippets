039d4f2492a5a6dcbc0b91c013680dc836f75409
Softsquirrel Studios <68344468+Squirrelcoding@users.noreply.github.com>
2021-04-06 17:15:35 -0500

    Update post.js

--- poopjokeAPI/post.js
+++ poopjokeAPI/post.js
@@ -1,16 +1,4 @@
 var axios = require("axios");
-var express = require('express');
-var cors = require('cors')
-var app = express();
-const bodyParser = require('body-parser');
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use(express.static('views'));
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
-function randint(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
 
 //Function to make things faster
 function axiosTest(options) {
